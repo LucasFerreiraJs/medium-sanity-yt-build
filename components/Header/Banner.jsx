b485e2847edb89f9ffdd663d218edb2e4e8814f7
thasuka <casferreiraa@gmail.com>
2022-07-14 06:08:51 -0300

    finished build

---
+++ components/Header/Banner.jsx
@@ -0,0 +1,25 @@
+
+
+export function Banner(){
+
+  return (
+    <div className="flex justify-between items-center bg-yellow-400 border-y border-black py-10 lg:py-0">
+      <div className="px-10 space-y-5">
+        <h1 className="text-6xl max-w-xl font-serif">
+          <span className="underline decoration-black decoration-4">Medium</span> is a place to write, read, and connect
+        </h1>
+
+        <h2 className="">
+          It's easy and free to post yout thinking on any topic and connect With millions of readers.
+        </h2>
+
+      </div>
+
+      <img className='hidden md:inline-flex h-32 lg:h-full'
+      src="/assets/images/m-logo.png" alt="" />
+
+    </div>
+
+  )
+
+}
---
+++ sanity.js
@@ -0,0 +1,27 @@
+
+import createImageUrlBuilder from '@sanity/image-url'
+
+import {
+  createCurrentUserHook,
+  createClient
+} from 'next-sanity'
+
+
+
+
+export const config = {
+
+
+  dataset: process.env.NEXT_PUBLIC_SANIOTY_DATASET || 'production',
+  projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
+  apiVersion: "2021-03-25",
+  useCdn: process.env.NODE_ENV === "production"
+
+
+};
+
+export const sanityClient = createClient(config);
+
+export const urlFor = (source) => createImageUrlBuilder(config).image(source);
+
+export const useCurrentUser = createCurrentUserHook(config);
